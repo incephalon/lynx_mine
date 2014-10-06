a4157951afcfa39dd2b3a79e252f873a808ce615
Yurij Sopila <yurij.sopila@gmail.com>
2014-10-06 23:06:07 +0300

    Updated packages. Arranged css and js

--- lynx_mine/Controllers/HomeController.cs
+++ lynx_mine/Controllers/HomeController.cs
@@ -1,34 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using DataAccess;
-using DataAccess.Model;
-using System.Web.Http;
 
 namespace lynx_mine.Controllers
 {
     public class HomeController : Controller
     {
-        //
-        // GET: /Home/
-
-		DataContext dbContext = new DataContext();
+        private DataContext _dbContext;
+        private DataContext DbContext
+        {
+            get { return _dbContext ?? (_dbContext = new DataContext()); }
+        }
 
         public ActionResult Index()
         {
             return View();
         }
-
-		public JsonResult Tags([FromUri]string term)
-		{
-			List<TagModel> tags = dbContext.Tags.Where(x => x.Name.Contains(term)).ToList();
-			if (!tags.Exists(x => x.Name == term))
-			{
-				tags.Add(new TagModel {Name = term });
-			}
-			return Json(tags,JsonRequestBehavior.AllowGet);
-		}
     }
 }
---
+++ lynx_mine/Controllers/TagController.cs
@@ -0,0 +1,28 @@
+﻿using DataAccess;
+using DataAccess.Model;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace lynx_mine.Controllers
+{
+    public class TagController: Controller
+    {
+        private DataContext _dbContext;
+        private DataContext DbContext 
+        {
+            get { return _dbContext ?? (_dbContext = new DataContext()); }
+        }
+
+        public JsonResult Index([FromUri]string term)
+        {
+            List<TagModel> tags = _dbContext.Tags.Where(x => x.Name.Contains(term)).ToList();
+            if (!tags.Exists(x => x.Name == term))
+            {
+                tags.Add(new TagModel { Name = term });
+            }
+            return Json(tags, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
